378629f534bb1e0e3766fb99f901ac48a1f9b3fb
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-05-14 15:43:06 +0500

    fix: remvoed unecessary code

--- apps/native/app/(authenticated)/dashboard/(drawer)/shared.tsx
+++ apps/native/app/(authenticated)/dashboard/(drawer)/shared.tsx
@@ -1,12 +1,13 @@
 import { RefreshableContent } from '@/components/common/refreshable-content'
 import { ThemedText } from '@/components/common/themed-text'
 import { ThemedView } from '@/components/common/themed-view'
-import App from '@/components/swipeable-example/example'
 
 export default function Page() {
 	return (
 		<RefreshableContent>
-			<App />
+			<ThemedView className='flex-1 items-center justify-center'>
+				<ThemedText>shared</ThemedText>
+			</ThemedView>
 		</RefreshableContent>
 	)
 }
