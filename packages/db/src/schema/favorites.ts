b3ef0f1faf84da764f405fd7009a7c1d75b5d2f9
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-05-14 15:39:09 +0500

    added schema for trash and favorites

--- packages/db/src/schema/favorites.ts
+++ packages/db/src/schema/favorites.ts
@@ -2,7 +2,7 @@ import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const favorites = pgTable('favorites', {
 	id: uuid('id').defaultRandom().primaryKey(),
-	file: text('file').notNull(),
+	file: uuid('file').notNull(),
 	user: text('user').notNull(),
 	createdAt: timestamp('created_at').defaultNow().notNull(),
 	updatedAt: timestamp('updated_at')
