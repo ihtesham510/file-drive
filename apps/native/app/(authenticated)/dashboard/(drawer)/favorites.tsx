daef630eab7bf8aec3d1c92b0fde9e0fa71270cb
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-05-15 12:31:14 +0500

    added favorites files list

--- apps/native/app/(authenticated)/dashboard/(drawer)/favorites.tsx
+++ apps/native/app/(authenticated)/dashboard/(drawer)/favorites.tsx
@@ -1,10 +1,16 @@
+import { StarOffIcon, Trash } from '@hugeicons/core-free-icons'
+import { useCSSVariable } from 'uniwind'
+import { RenderUnderIcon } from '@/components/common/render-under-icons'
 import { FilesList } from '@/components/dashboard/files-list'
+import { OFF_SET, SNAP_POINT } from '@/constants'
 import { useFavorites } from '@/hooks/use-favorites'
 import { useTrash } from '@/hooks/use-trash'
 
-// TODO: add other options here
-
 export default function Page() {
+	const [rightIconColor, leftIconColor] = useCSSVariable([
+		'--color-destructive-foreground',
+		'--color-primary-foreground',
+	]) as string[]
 	const { favFiles, toggle } = useFavorites()
 	const { add: addToTrash } = useTrash()
 	const files = favFiles.data?.map(file => file.file)
@@ -16,6 +22,46 @@ export default function Page() {
 				createdAt: new Date(file.createdAt),
 				updatedAt: new Date(file.updatedAt),
 			}))}
+			underLeftContent={(_, _props) => {
+				return (
+					<RenderUnderIcon
+						offset={OFF_SET}
+						side='left'
+						icon={StarOffIcon}
+						viewProps={{
+							className: 'bg-primary',
+						}}
+						textProps={{
+							className: 'text-primary-foreground',
+						}}
+						text={'Remove From Favorites'}
+						iconColor={leftIconColor}
+						buttonProps={_props}
+						snapPoint={SNAP_POINT}
+					/>
+				)
+			}}
+			underRightContnet={(_, _props) => {
+				return (
+					<RenderUnderIcon
+						offset={OFF_SET}
+						side='right'
+						icon={Trash}
+						viewProps={{
+							className: 'bg-destructive',
+						}}
+						textProps={{
+							className: 'text-destructive-foreground',
+						}}
+						text='Remove From Favorites'
+						iconColor={rightIconColor}
+						buttonProps={_props}
+						snapPoint={SNAP_POINT}
+					/>
+				)
+			}}
+			shouldExpandLeft={() => true}
+			shouldExpandRight={() => true}
 			onPressRight={file => addToTrash(file.id)}
 			onPressLeft={file => toggle(file.id)}
 		/>
---
+++ apps/native/constants.ts
@@ -0,0 +1,2 @@
+export const SNAP_POINT = 150
+export const OFF_SET = 8
