806280c0bc08ab93f76b7e0bc45a10d28df3c26b
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-06-04 02:26:08 +0500

    renamed output folder name in tsdown.config.ts

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
 	entry: './src/index.ts',
-	format: 'esm',
-	outDir: './dist',
+	format: 'module',
+	outDir: './public',
 	clean: true,
 	noExternal: [/@file-drive\/.*/],
 })
