255cb3a0ea3d2c0756071eb9e0e4c4f2dce7cba3
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-06-03 22:46:45 +0500

    added vite proxy

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -1,5 +1,4 @@
 import type { AppRouter } from '@file-drive/api/routers/index'
-import { env } from '@file-drive/env/web'
 import { QueryCache } from '@tanstack/react-query'
 import { createTRPCClient, httpBatchLink } from '@trpc/client'
 import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query'
@@ -28,7 +27,7 @@ export const queryClient = getQueryClient({
 export const trpcClient = createTRPCClient<AppRouter>({
 	links: [
 		httpBatchLink({
-			url: `${env.VITE_SERVER_URL}/trpc`,
+			url: '/trpc',
 			fetch(url, options) {
 				return fetch(url, {
 					...options,
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,27 +1,34 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-	server: {
-		port: 3001,
-		proxy: {
-			'/api': {
-				changeOrigin: true,
-				target: 'http://localhost:3000',
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	return {
+		server: {
+			port: 3001,
+			proxy: {
+				'/trpc': {
+					changeOrigin: true,
+					target: env.VITE_SERVER_URL,
+				},
+				'/api': {
+					changeOrigin: true,
+					target: env.VITE_SERVER_URL,
+				},
 			},
 		},
-	},
-	resolve: {
-		tsconfigPaths: true,
-	},
-	plugins: [
-		tailwindcss(),
-		tanstackRouter({
-			target: 'react',
-			autoCodeSplitting: true,
-		}),
-		react(),
-	],
+		resolve: {
+			tsconfigPaths: true,
+		},
+		plugins: [
+			tailwindcss(),
+			tanstackRouter({
+				target: 'react',
+				autoCodeSplitting: true,
+			}),
+			react(),
+		],
+	}
 })
