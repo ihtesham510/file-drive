a200c1849720c0c5f006047ae142329bab8bd68c
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-06-03 12:27:38 +0500

    added proxy

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,18 +2,18 @@ import { createContext } from '@file-drive/api/context'
 import { appRouter } from '@file-drive/api/routers/index'
 import { auth } from '@file-drive/auth'
 import { env } from '@file-drive/env/server'
-import { getS3Client, setPublic } from '@file-drive/s3'
 import { trpcServer } from '@hono/trpc-server'
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import proxyRoute from './proxy'
 import uploadRoute from './upload'
 
 const app = new Hono()
-const s3Client = getS3Client()
 
 app.use(logger())
+app.use('/*', serveStatic({ root: '../web/dist' }))
 app.use(
 	'/*',
 	cors({
@@ -38,16 +38,6 @@ app.use(
 
 app.route('/upload', uploadRoute)
 app.route('/file-drive', proxyRoute)
-
-app.get('/', async c => {
-	try {
-		await setPublic(s3Client, 'file-drive')
-		console.log('policy is set')
-		return c.text('OK')
-	} catch {
-		return c.text('Error')
-	}
-})
 
 Bun.serve({
 	fetch: app.fetch,
