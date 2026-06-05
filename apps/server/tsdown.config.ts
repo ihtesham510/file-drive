5d0685c952528a50c6908e367a1564bb6f92686b
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-06-05 17:24:41 +0500

    renamed out put folder from public to dist

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
 	entry: './src/index.ts',
 	format: 'module',
-	outDir: './public',
+	outDir: './dist',
 	clean: true,
 	noExternal: [/@file-drive\/.*/],
 })
